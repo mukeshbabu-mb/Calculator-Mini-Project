6d27bd839886632c02196813d38cb6d3fb0481bc
mukeshbabu-mb <mukeshbabu6671@gmail.com>
2024-01-29 21:50:47 +0530

    version 1.0

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function calculate(opt){
+    var a=document.getElementById("num1").value;
+    var b=document.getElementById("num2").value;
+    var res=0;
+    switch (opt) {
+        case 'add':
+            res=a+b;
+            break;
+        case 'sub':
+            res=a-b;
+            break;
+        case 'mul':
+            res=a*b;
+            break;
+        case 'div':
+            res=a/b;
+            break;
+        default:
+            break;
+    }
+    document.getElementById("result").innerHTML = res;
+
+}
